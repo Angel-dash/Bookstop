12636ec5a330f5d893932dad40b14525217f75a2
Angel-dash <njlghmr@gmail.com>
2022-08-05 22:17:26 +0545

    first commit

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./Home";
+import Signup from "./Signup";
+import Signin from "./Signin";
+
+const Navbar = () => {
+	return (
+		<>
+			<Home />
+			<Signin />
+			<Signup />
+		</>
+	);
+};
+
+export default Navbar;
