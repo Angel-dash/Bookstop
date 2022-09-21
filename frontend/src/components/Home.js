3c4f2d07690d2aaaf8f1333d9064940ace1c314d
Angel-dash <njlghmr@gmail.com>
2022-09-21 16:19:21 +0545

    editos choice charousel

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Carousel from "./Carousel";
+import SimpleSlider from "./EditorsChoice";
 const Home = () => {
 	return (
 		<>
+			<SimpleSlider />
 			<Carousel />
 			<Carousel />
 			<Carousel />
--- frontend/src/components/data.js
+++ frontend/src/components/data.js
@@ -39,4 +39,46 @@ export const dataBooks = [
 			"https://c4.wallpaperflare.com/wallpaper/745/141/70/movie-the-fault-in-our-stars-ansel-elgort-shailene-woodley-wallpaper-preview.jpg",
 		title: "The fault in our stars",
 	},
+	{
+		id: 6,
+		category: "Bio-pics",
+		price: "72",
+		linkImg:
+			"https://c4.wallpaperflare.com/wallpaper/581/868/873/steve-jobs-face-wallpaper-preview.jpg",
+		title: "Pirates of the Silicon valley ",
+	},
+	{
+		id: 7,
+		category: "Bio-pics",
+		price: "65",
+		linkImg:
+			"https://c0.wallpaperflare.com/preview/214/870/349/communication-connection-network-social.jpg",
+		title: "The social Network  ",
+	},
+];
+export const editorChoice = [
+	{
+		id: 1,
+		category: "Fantasy",
+		price: "75",
+		linkImg:
+			"https://c4.wallpaperflare.com/wallpaper/539/250/327/game-of-thrones-daenerys-targaryen-dragon-fire-wallpaper-preview.jpg",
+		title: "The Songs of Ice and Fire",
+	},
+	{
+		id: 2,
+		category: "Bio-pics",
+		price: "75",
+		linkImg:
+			"https://c4.wallpaperflare.com/wallpaper/581/868/873/steve-jobs-face-wallpaper-preview.jpg",
+		title: "Steve Jobs",
+	},
+	{
+		id: 3,
+		category: "Sci-fi",
+		price: "75",
+		linkImg:
+			"https://c4.wallpaperflare.com/wallpaper/996/743/625/pascal-blanche-dune-movie-dune-series-artwork-science-fiction-hd-wallpaper-preview.jpg",
+		title: "Dune",
+	},
 ];
