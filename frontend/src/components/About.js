dc16fba5ee130980cc8df057b1244e53e6d9af4e
Angel-dash <njlghmr@gmail.com>
2022-10-15 20:02:25 +0545

    Carousel on home page with categor

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
-	return <div className="container-a">About</div>;
+	return (
+		<div className="acontainer">
+			<div className="aslider">
+				<img src="https://via.placeholder.com/210/00FF00?text=1" />
+				<img src="https://via.placeholder.com/220/00FF00?text=2" />
+				<img src="https://via.placeholder.com/230/00FF00?text=3" />
+				<img src="https://via.placeholder.com/240/00FF00?text=4" />
+				<img src="https://via.placeholder.com/250/00FF00?text=5" />
+				<img src="https://via.placeholder.com/260/00FF00?text=6" />
+				<img src="https://via.placeholder.com/270/00FF00?text=7" />
+				<img src="https://via.placeholder.com/280/00FF00?text=8" />
+				<img src="https://via.placeholder.com/290/00FF00?text=9" />
+			</div>
+		</div>
+	);
 };
 
 export default About;
